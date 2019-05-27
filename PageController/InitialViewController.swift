275334340b7a9b4dcae2ec17861556349bcb0a64
Balvant Singh Chauhan <Balvant@FVFXQ286HV27.local>
2019-05-27 15:32:30 +0530

    4.2 swift version support

---
+++ PageController/InitialViewController.swift
@@ -0,0 +1,28 @@
+//
+//  InitialViewController.swift
+//  PageController
+//
+//  Created by Balvant Singh Chauhan on 20/05/19.
+//  Copyright © 2019 Balvant Singh Chauhan. All rights reserved.
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PageController/WizardPageContainerVC.swift
@@ -0,0 +1,67 @@
+//
+//  WizardPageContainerVC.swift
+//  PageController
+//
+//  Created by Balvant Singh Chauhan on 20/05/19.
+//  Copyright © 2019 Balvant Singh Chauhan. All rights reserved.
+
+import UIKit
+
+class WizardPageContainerVC: UIViewController {
+    @IBOutlet weak var wizardControllsView: PageControlls!
+    var pageCount:Int = 0
+    var pageViewController:PageViewController?
+    
+    
+    //*******************************************************
+    // MARK: -  Life cycle method
+    // MARK: -
+    //*******************************************************
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        wizardControllsView.wizardDelegate  =   self
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let pageVC = segue.destination as? PageViewController {
+            pageViewController  =   pageVC
+            pageVC.pageDelegate = self
+        }
+    }
+}
+//*******************************************************
+// MARK: -  ClassExtension
+// MARK: -
+//*******************************************************
+extension WizardPageContainerVC: PageViewControllerDelegate {
+    func pageViewController(pageViewController: PageViewController, didUpdatePageCount count: Int) {
+        if count > 1{
+            wizardControllsView.progressNextButton.isHidden =   false
+        }else{
+            wizardControllsView.progressNextButton.isHidden =   true
+        }
+        pageCount   =   count        
+    }
+    func pageViewController(pageViewController: PageViewController, didUpdatePageIndex index: Int) {
+        wizardControllsView.wizardState(index: index, totalPage: pageCount)
+    }
+}
+
+extension WizardPageContainerVC : WizardControlsDelegate{
+    func nextWizard() {
+        if let pageController = pageViewController{
+            pageController.movePageForword()
+        }
+    }
+    func perviousWizard() {
+        if let pageController = pageViewController{
+            pageController.movePageBackword()
+        }
+    }
+    func skipWizard() {
+        
+    }
+    
+    func compliteWizard() {
+        
+    }
+}
